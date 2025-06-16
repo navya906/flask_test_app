ec00a2f4ed712010c01dcca8f7d2178a8ddfe4fb
navya906 <navyaghatta@gmail.com>
2025-06-16 16:16:22 +0530

    Delete new.py

--- new.py
+++
@@ -1,8 +0,0 @@
-#make a testing system
-#admin can create tests and view results 
-#user can give tests 
-#admin can make a question bank
-#from which they can select questions for tests
-#results should be immediately available to the user
-# mcqs based on the question bank
-#keep all login wala stuff
